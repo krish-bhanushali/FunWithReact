013ff0afe49e15e7f1e6df2dfbd4147b69206312
krish-bhanushali <bhanushali123krish@gmail.com>
2021-05-01 16:08:08 +0530

    creaded GlobalContext for tapping into ContextAPi for statemanagement

--- playWithThemes/gatsby-browser.js
+++ playWithThemes/gatsby-browser.js
@@ -1,7 +1,8 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
 
-// You can delete this file if you're not using it
+import React from "react"
+
+import {GlobalProvider} from './src/context/globalContext'
+
+export const wrapRootElement = ({element}) => {
+    return <GlobalProvider>{element}</GlobalProvider>
+}
---
+++ playWithThemes/src/context/globalContext.js
@@ -0,0 +1,42 @@
+import React , {createContext, useReducer, useContext} from "react";
+
+//Define context
+const GlobalStateContext = createContext();
+const GlobalDispatchContext = createContext();
+
+//Reducer
+
+const globalReducer = (state, action) => {
+    switch(action.type) {
+        case 'TOGGLE_THEME' : {
+            
+            return {
+                ...state,
+                currentTheme : action.theme } 
+        }
+        default : {
+            throw new Error(`Unhandled action type: ${action.type}`)
+        }
+    }
+}
+
+export const GlobalProvider = ( {children}) => {
+    const [state, dispatch] = useReducer(globalReducer, {
+        currentTheme : window.localStorage.getItem('theme') == null ? 'dark' : window.localStorage.getItem('theme')
+    })
+
+    return (
+        <GlobalDispatchContext.Provider value={dispatch} >
+            <GlobalStateContext.Provider value={state}>
+                {children}
+            </GlobalStateContext.Provider>
+
+        </GlobalDispatchContext.Provider>
+
+    )
+}
+
+
+export const useGlobalStateContext = () => useContext(GlobalStateContext)
+
+export const useGlobalDispatchContext = () => useContext(GlobalDispatchContext)
