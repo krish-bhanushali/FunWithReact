c237b5612062075b3a84acfc3e61fca0fc9e08bc
krish-bhanushali <bhanushali123krish@gmail.com>
2021-05-01 15:30:53 +0530

    added Global Theme Provider and themes

---
+++ playWithThemes/src/components/layout.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+
+//styled Components
+//Global Style will provide an overall style to our components
+//Normalize basically removes any additional margin or padding by browser and sets it to zero
+//Theme provider will basically provide us theme to embedded components which can later be altered
+import {createGlobalStyle, ThemeProvider} from 'styled-components';
+import {normalize} from 'styled-normalize';
+
+
+
+//global Style you can set your fonts and background here
+//we access the props by theme provider 
+const GlobalStyle = createGlobalStyle`
+${normalize}
+  *{
+    text-decoration: none;
+    /* cursor: none; */
+  }
+
+  html {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    font-size: 16px;
+
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    background: ${props => props.theme.background};
+    overscroll-behavior: none;
+    overflow-x: hidden;
+  }
+
+`
+
+
+
+
+
+
+
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  const darkTheme = {
+    background:'#000',
+    text:'#fff',
+  
+  }
+
+  const lightTheme = {
+    background:'#fff',
+    text:'#000',
+ 
+  }
+
+  return (<>
+  <ThemeProvider theme={darkTheme}>
+  <GlobalStyle></GlobalStyle>
+  <main>{children}</main>
+  </ThemeProvider>
+ 
+  </>)
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
